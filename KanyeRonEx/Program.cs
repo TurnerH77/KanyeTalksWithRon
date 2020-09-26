2dc1330bb7ade9812fc21ce82f8d43d79aff9b95
turner harris <turnerharris7798@yahoo.com>
2020-09-25 20:25:23 -0400

    finished Project

---
+++ KanyeRonEx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace KanyeRonEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var kwote = new QuoteGetter(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye - {kwote.KanyeGetter()}");
+                Console.WriteLine($"Ron - {kwote.RonGetter()}");
+            }
+        }
+    }
+}
---
+++ KanyeRonEx/QuoteGetter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+public class QuoteGetter
+{
+	public HttpClient _client;
+	public QuoteGetter(HttpClient client)
+	{
+		_client = client;
+	}
+
+	HttpClient client = new HttpClient();
+
+	public string KanyeGetter()
+    {
+		var kanyeURL = "https://api.kanye.rest";
+		var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+		var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+		return kanyeQuote;
+	}
+
+	public string RonGetter()
+    {
+		var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+		var ronResponse = _client.GetStringAsync(ronURL).Result;
+		var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+		return ronQuote;
+	}
+}
